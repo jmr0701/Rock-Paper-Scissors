ff39ddc33c75b259c29756cd3dd0c745d9b07ad8
Jose Munoz <jmrsocials1@gmail.com>
2025-10-19 09:50:46 -0400

    Write function to play the game for one round

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -2,6 +2,64 @@ let humanScore = 0;
 let computerScore = 0;
 let tiedScore = 0;
 
+function playRound (){
+    let computerSelection = getComputerChoice().toLowerCase();
+    let playerSelection = getHumanChoice().toLowerCase();
+
+
+    if (playerSelection === computerSelection){
+        console.log("You played: " + playerSelection);
+        console.log("The computer played: " + computerSelection);
+        console.log("It's a tie!");
+        tiedScore++
+    }
+
+    if (playerSelection === "rock" && computerSelection === "paper"){
+        console.log("You played: " + playerSelection);
+        console.log("The computer played: " + computerSelection);
+        console.log("Computer wins!");
+        computerScore++;
+
+    }
+    if (playerSelection === "rock" && computerSelection === "scissors"){
+        console.log("You played: " + playerSelection);
+        console.log("The computer played: " + computerSelection);
+        console.log("You win!");
+        humanScore++;
+    }
+
+    if (playerSelection === "paper" && computerSelection === "rock"){
+        console.log("You played: " + playerSelection);
+        console.log("The computer played: " + computerSelection);
+        console.log("You win!");
+        humanScore++;
+    }
+    if (playerSelection === "paper" && computerSelection === "scissors"){
+        console.log("You played: " + playerSelection);
+        console.log("The computer played: " + computerSelection);
+        console.log("Computer wins!");
+        computerScore++;
+    }
+
+    if (playerSelection === "scissors" && computerSelection === "rock"){
+        console.log("You played: " + playerSelection);
+        console.log("The computer played: " + computerSelection);
+        console.log("Computer wins!");
+        computerScore++;
+    }
+    if (playerSelection === "scissors" && computerSelection === "paper"){
+        console.log("You played: " + playerSelection);
+        console.log("The computer played: " + computerSelection);
+        console.log("You win!");
+        humanScore++;
+    }
+
+    console.log ("Your score is: " + humanScore);
+    console.log ("The computer's score is: " + computerScore);
+    console.log ("You and the computer tied: " + tiedScore + " times")
+
+}
+
 function getComputerChoice (){
     let number = Math.floor(Math.random() * 3);
     if (number === 0){
