8da585f58921cafa2044c0fe3356bcffddb64cdd
Jose Munoz <jmrsocials1@gmail.com>
2025-10-22 17:24:11 -0400

    Add buttons with functionality and linked css file

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -2,6 +2,21 @@ let humanScore = 0;
 let computerScore = 0;
 let tiedScore = 0;
 
+const btnRock = document.querySelector("#btnRock");
+btnRock.addEventListener("click", function(e) {
+  playRound("Rock");
+});
+
+const btnPaper = document.querySelector("#btnPaper");
+btnPaper.addEventListener("click", function(e) {
+  playRound("Paper");
+});
+
+const btnScissors = document.querySelector("#btnScissors");
+btnScissors.addEventListener("click", function(e) {
+  playRound("Scissors");
+});
+
 function playGame (){
 
     for (i=0; i<5; i++){
@@ -17,9 +32,9 @@ function playGame (){
     }
 }
 
-function playRound (){
-    let computerSelection = getComputerChoice().toLowerCase();
-    let playerSelection = getHumanChoice().toLowerCase();
+function playRound (btnSelection){
+    let computerSelection = getComputerChoice();
+    let playerSelection = btnSelection;
 
 
     if (playerSelection === computerSelection){
@@ -29,40 +44,40 @@ function playRound (){
         tiedScore++
     }
 
-    if (playerSelection === "rock" && computerSelection === "paper"){
+    if (playerSelection === "Rock" && computerSelection === "Paper"){
         console.log("You played: " + playerSelection);
         console.log("The computer played: " + computerSelection);
         console.log("Computer wins!");
         computerScore++;
 
     }
-    if (playerSelection === "rock" && computerSelection === "scissors"){
+    if (playerSelection === "Rock" && computerSelection === "Scissors"){
         console.log("You played: " + playerSelection);
         console.log("The computer played: " + computerSelection);
         console.log("You win!");
         humanScore++;
     }
 
-    if (playerSelection === "paper" && computerSelection === "rock"){
+    if (playerSelection === "Paper" && computerSelection === "Rock"){
         console.log("You played: " + playerSelection);
         console.log("The computer played: " + computerSelection);
         console.log("You win!");
         humanScore++;
     }
-    if (playerSelection === "paper" && computerSelection === "scissors"){
+    if (playerSelection === "Paper" && computerSelection === "Scissors"){
         console.log("You played: " + playerSelection);
         console.log("The computer played: " + computerSelection);
         console.log("Computer wins!");
         computerScore++;
     }
 
-    if (playerSelection === "scissors" && computerSelection === "rock"){
+    if (playerSelection === "Scissors" && computerSelection === "Rock"){
         console.log("You played: " + playerSelection);
         console.log("The computer played: " + computerSelection);
         console.log("Computer wins!");
         computerScore++;
     }
-    if (playerSelection === "scissors" && computerSelection === "paper"){
+    if (playerSelection === "Scissors" && computerSelection === "Paper"){
         console.log("You played: " + playerSelection);
         console.log("The computer played: " + computerSelection);
         console.log("You win!");
@@ -86,9 +101,4 @@ function getComputerChoice (){
     }
 };
 
-function getHumanChoice (){
-    playerChoice = prompt ("Rock, paper or scissors?");
-    return playerChoice;
-}
-
-playGame()
+//playGame()
